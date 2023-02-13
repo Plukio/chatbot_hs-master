03adbfd0fef27cc9d8971ca2ac0efc3896c47bd5
Pluk170 <112405935+Pluk170@users.noreply.github.com>
2023-02-13 15:31:44 +0700

    Data

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 import telebot
-import openai
 import os
 
 bot = telebot.TeleBot(os.environ.get("TELEGRAM_TOKEN"))
-openai.api_key = os.environ.get("OPENAI_TOKEN")
 
-
+    
 @bot.message_handler(commands=['start'])
-def start(m, res=False):
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="what is an elephant?",
-        max_tokens=2048,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    ).choices[0].text
-    bot.send_message(m.chat.id, response)
+def start_handler(message):
+    predicted_orders =  predict_orders(model)
+    bot.send_message(chat_id = message.chat.id, text=f"The predicted number of orders is {predicted_orders}")
 
 
 # Press the green button in the gutter to run the script.
